a8f6c09dfa31ffc74ad079810249349aa7073cbe
NICOWEB-KOZUE <mew0119@gmail.com>
2025-09-30 22:32:18 +0900

    制御文の埋め込み_if

---
+++ 02_form2.php
@@ -0,0 +1,49 @@
+<?php
+
+$score = '';
+$err_msg = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $score = $_POST['score'];
+    if (empty($score)) {
+        $err_msg = '点数が入力されていません。';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>埋め込み</title>
+</head>
+
+<body>
+
+    <?php if ($_SERVER['REQUEST_METHOD'] !== 'POST'): ?>
+        <h1>点数を入力してください</h1>
+    <?php else: ?>
+        <?php if ($err_msg !== ''): ?>
+            <h1>点数を入力してください</h1>
+            <ul>
+                <li><?= $err_msg ?></li>
+            </ul>
+        <?php else: ?>
+            <?php if ($score >= 60): ?>
+                <h1>合格です</h1>
+            <?php else: ?>
+                <h1>不合格です</h1>
+            <?php endif; ?>
+        <?php endif; ?>
+    <?php endif; ?>
+
+    <form action="" method="post">
+        <input type="number" name="score">
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
